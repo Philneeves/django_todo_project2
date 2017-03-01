e0e9f3d28afc5ca3cf440d61410e1a969d5fa3cf
Philneeves <p.neeves@gmail.com>
2017-03-01 15:05:51 +0000

    put in asterisk url to allowed hosts

--- settings/staging.py
+++ settings/staging.py
@@ -1,4 +1,5 @@
 from base import *
+import dj_database_url
 
 DEBUG = True
 
@@ -8,5 +9,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+DATABASES['default'] = dj_database_url.config("mysql://bd54095e4f3b88:0075d804@eu-cdbr-west-01.cleardb.com/heroku_97be905cae79e24")
 
 SITE_URL = 'https://git.heroku.com/django-todo2.git'
